65802da0ef18af0af913406ede19ceffad546203
Junier Oliva <joliva@cs.cmu.edu>
2014-08-07 12:24:57 -0400

    get opt defualt param is optional

--- misc/get_opt.m
+++ misc/get_opt.m
@@ -1,7 +1,11 @@
-function opt = get_opt(opts, name, default)
-if isfield(opts,name)
-    opt = opts.(name);
-else
-    opt = default;
-end
+function opt = get_opt(opts, name, varargin)
+    default = [];
+    if ~isempty(varargin)
+        default = varargin{1};
+    end
+    if isfield(opts,name)
+        opt = opts.(name);
+    else
+        opt = default;
+    end
 end
