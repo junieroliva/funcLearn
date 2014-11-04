e4adc48953d72cc2f38667979e9c314eb4e3c063
Junier Oliva <joliva@cs.cmu.edu>
2014-11-04 17:19:32 -0500

    cv bandwidth using ISE

--- FFR/gauss_norma.m
+++ FFR/gauss_norma.m
@@ -1,8 +1,28 @@
-function norma = gauss_norma(x, sigma2, trunc)
-    [n,d] = size(x);
-    if trunc
-        norma = 1./((2*pi*sigma2)^(d/2)*prod(normcdf((1-x)/sqrt(sigma2))-normcdf(-x/sqrt(sigma2)),2)*n);
-    else
-        norma = 1./((2*pi*sigma2)^(d/2)*ones(n,1)*n);
-    end
+function norma = gauss_norma(x, sigma2, trunc, varargin)
+% Computes normalizing constants to exponential bumps located at points x
+% for KDE
+%   Inputs -
+%   x: Points KDE is based on (n x d)
+%   sigma2: bandwidth for KDE
+%   trunc: boolean indicating whether to use kernels of truncated
+%           normals (true) or untruncated normals (false)
+%   a (optional), b (optional): [a,b]^d cube containing support [0,1]^d by
+%       default
+
+[n,d] = size(x);
+
+if length(varargin)>=2
+    a = varargin{1};
+    b = varargin{2};
+else
+    a = 0;
+    b = 1;
+end
+
+if trunc
+    norma = 1./((2*pi*sigma2)^(d/2)*prod(normcdf((b-x)/sqrt(sigma2))-normcdf((a-x)/sqrt(sigma2)),2)*n);
+else
+    norma = 1./((2*pi*sigma2)^(d/2)*ones(n,1)*n);
+end
+
 end
--- FFR/kde_gauss.m
+++ FFR/kde_gauss.m
@@ -1,12 +1,16 @@
 function [p, norma, sigma2] = kde_gauss( x, varargin )
-%kde_gauss  KDE based on points x, evaluated at points xe.
+% KDE based on points x, evaluated at points xe.
 %   Inputs -
-%   xe: Points KDE will be evaluated at
 %   x: Points KDE is based on
+%   xe (optional): Points KDE will be evaluated at (xe = x if xe not
+%       provided)
 %   opts (optional): a struct of options with the following possible fields
 %       sigma2: bandwidth to use
 %       trunc: boolean indicating whether to use kernels of truncated
 %           normals (true) or untruncated normals (false)
+%       CV_ISE: CV bandwidth using integrated squared error if sigma2 not
+%           provided and CV_ISE is true, or CV using negative log
+%           likelihood if sigma2 not provided and CV_ISE is false
 
 if ~isempty(varargin)
     xe = varargin{1};
@@ -22,20 +26,28 @@ end
 [n,d] = size(x);
 trunc = get_opt(opts, 'trunc', true);
 sigma2 = get_opt(opts, 'sigma2', nan);
+CV_ISE = get_opt(opts, 'CV_ISE', true);
 
-if isnan(sigma2) % cross-validate bandwidth based on LOO-LL
-                 % TODO: cv based on IMSE
+if isnan(sigma2) % cross-validate bandwidth 
     D2xx = slmetric_pw(x',x','sqdist');
     sigma2_rot = mean(mean(D2xx));
-    mult = get_opt(opts, 'mult', 2.^(-10:10));
-    ll = nan(length(mult),1);
+    mult = get_opt(opts, 'mult', 2.^(-20:10));
+    scores = nan(length(mult),1);
     for mi=1:length(mult)
         sigma2 = sigma2_rot*mult(mi);
+        % compute leave one out density estimates
         norma = gauss_norma(x,sigma2,trunc);
         px = (n/(n-1))*(exp(-D2xx/(2*sigma2))*norma-norma);
-        ll(mi) = sum(log(px));
+        % if CVing ISE, compute the L2 norm squared
+        if CV_ISE
+            C = gauss_prod_C(x, sigma2, x, sigma2, trunc);
+            p2 = sum(sum(exp(-D2xx/(4*sigma2)).*C.*(norma*norma')));
+            scores(mi) = p2 - 2*mean(px);
+        else % use negative LL score
+            scores(mi) = -sum(log(px));
+        end
     end
-    [~, mi] = max(ll);
+    [~, mi] = min(scores);
     sigma2 = sigma2_rot*mult(mi);
 end
 
