8129ae1886c4fa6f9695482d5923d5f10f177f2d
Junier Oliva <joliva@cs.cmu.edu>
2015-02-23 15:27:00 -0500

    ROT CV funcs in case of cells

--- func_est/osfe.m
+++ func_est/osfe.m
@@ -1,6 +1,5 @@
 function [pcs, inds, PM, yhat] = osfe(x, y, varargin)
 [N,~,p] = size(y);
-y = permute(y,[2 3 1]);
 d = size(x,2);
 
 if isempty(varargin)
@@ -13,20 +12,26 @@ N_rot = get_opt(opts, 'N_rot', min(N,20));
 p_rot = get_opt(opts, 'p_rot', min(p,5));
 
 if ~iscell(x) 
+    y = permute(y,[2 3 1]);
     inds = get_opt(opts, 'inds');
     if isempty(inds)
-        cv_norms = nan(N_rot, p_rot);
-        irprm = randperm(N,N_rot);
-        jrprm = randperm(p,p_rot);
-        for ii=1:N_rot
-            for jj=1:p_rot
-                [~,~,inds] = cv_os(x,squeeze(y(:,jrprm(jj),irprm(ii))),opts);
-                cv_norms(ii,jj) = max(sqrt(sum(inds.^2,2)));
+        max_norm = get_opt(opts, 'max_norm');
+        if isempty(max_norm)
+            cv_norms = nan(N_rot, p_rot);
+            irprm = randperm(N,N_rot);
+            jrprm = randperm(p,p_rot);
+            for ii=1:N_rot
+                for jj=1:p_rot
+                    [~,~,inds] = cv_os(x,squeeze(y(:,jrprm(jj),irprm(ii))),opts);
+                    cv_norms(ii,jj) = max(sqrt(sum(inds.^2,2)));
+                end
             end
+            max_norm = mean(cv_norms(:));
         end
-
-        max_norm = mean(cv_norms(:));
+        
         inds = outerprodinds(0:max_norm,d,max_norm);
+        inds = sortrows( [ sum(inds.^2,2), inds] );
+        inds = inds(:, 2:end);
     end
     phix = eval_basis(x,inds);
     PM = (phix'*phix) \ phix';
@@ -44,6 +49,24 @@ if ~iscell(x)
     
 else
     [cN,p] = size(x);
+    
+    inds = get_opt(opts, 'inds');
+    if isempty(inds)
+        max_norm = get_opt(opts, 'max_norm');
+        if isempty(max_norm)
+            cv_norms = nan(N_rot, p_rot);
+            irprm = randperm(N,N_rot);
+            jrprm = randperm(p,p_rot);
+            for ii=1:N_rot
+                for jj=1:p_rot
+                    [~,inds] = osfe(x{irprm(ii),jrprm(jj)}, y{irprm(ii),jrprm(jj)}, opts);
+                    cv_norms(ii,jj) = max(sqrt(sum(inds.^2,2)));
+                end
+            end
+            opts.max_norm = mean(cv_norms(:));
+        end
+    end
+    
     pcs = cell(cN,p);
     for i=1:cN
         for j=1:p
