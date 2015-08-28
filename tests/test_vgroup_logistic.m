39b397020cce4936f042bdeb05754727efd5c95b
Junier Oliva <joliva@bender.gp.cs.cmu.edu>
2015-08-28 13:26:52 -0400

    tested code to do group penalized logistic regression

--- tests/test_vgroup_logistic.m
+++ tests/test_vgroup_logistic.m
@@ -9,7 +9,7 @@ gsizes = randi(msize,p,1);
 ginds = cumsum(gsizes);
 
 X = randn(10,ginds(end));
-Y = X*randn(ginds(end),1);
+Y = 1./(1+exp(-X*randn(ginds(end),1)))>.5;
 opts = struct;
 opts.verbose = false;
 opts.intercept = false;
@@ -28,15 +28,18 @@ npadind = ~padind;
 cvx_begin quiet
     variable betal(msize,p)
 
-    minimize( .5*sum(sum_square(Y-Xpad*betal(:))) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) )
+    minimize( -Y'*Xpad*betal(:) +sum(log_sum_exp([zeros(1,N); betal(:)'*Xpad'])) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) )
 
     subject to
         betal(npadind) == 0
 cvx_end
 
-cvx_obj1 = .5*sum(sum_square(Y-Xpad*betal(:))) + lambda*dot(sqrt(gsizes),norms(betal,2,1));
+cvx_obj1 = -Y'*Xpad*betal(:) +sum(log_sum_exp([zeros(1,N); betal(:)'*Xpad'])) + lambda*dot(sqrt(gsizes),norms(betal,2,1));
 opt_obj1 = objs(rli);
-abs(opt_obj1-cvx_obj1)/cvx_obj1
+ratio1 = abs(opt_obj1-cvx_obj1)/cvx_obj1
+if ratio1>1E-6
+    error('Unacceptable objective ratio');
+end
 
 
 % test with intercept
@@ -50,16 +53,18 @@ lambda = lambdas(rli);
 cvx_begin quiet
     variables betal(msize,p) b(1,1)
 
-    minimize( .5*sum(sum_square(Y-Xpad*betal(:)-b)) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) )
+    minimize( -Y'*(Xpad*betal(:)+b) +sum(log_sum_exp([zeros(1,N); betal(:)'*Xpad'+b])) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) )
 
     subject to
         betal(npadind) == 0
 cvx_end
 
-cvx_obj2 = .5*sum(sum_square(Y-Xpad*betal(:)-b)) + lambda*dot(sqrt(gsizes),norms(betal,2,1));
+cvx_obj2 = -Y'*(Xpad*betal(:)+b) +sum(log_sum_exp([zeros(1,N); betal(:)'*Xpad'+b])) + lambda*dot(sqrt(gsizes),norms(betal,2,1));
 opt_obj2 = objs(rli);
-abs(opt_obj2-cvx_obj2)/cvx_obj2
-
+ratio2 = abs(opt_obj2-cvx_obj2)/cvx_obj2
+if ratio2>1E-6
+    error('Unacceptable objective ratio');
+end
 
 % test with l1 sparsity and elastic net lambdas
 opts = struct;
@@ -76,21 +81,24 @@ lambdae = opts.lambdae;
 cvx_begin quiet
     variables betal(msize,p)
 
-    minimize( .5*sum(sum_square(Y-Xpad*betal(:))) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) ...
+    minimize( -Y'*Xpad*betal(:) +sum(log_sum_exp([zeros(1,N); betal(:)'*Xpad'])) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) ...
                 + lambda1*sum(abs(betal(:))) + (lambdae/2)*sum_square(betal(:)) )
 
     subject to
         betal(npadind) == 0
 cvx_end
 
-cvx_obj3 = .5*sum(sum_square(Y-Xpad*betal(:))) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) ...
+cvx_obj3 = -Y'*Xpad*betal(:) +sum(log_sum_exp([zeros(1,N); betal(:)'*Xpad'])) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) ...
                 + lambda1*sum(abs(betal(:))) + (lambdae/2)*sum_square(betal(:));
 opt_obj3 = objs(rli);
-abs(opt_obj3-cvx_obj3)/cvx_obj3
+ratio3 = abs(opt_obj3-cvx_obj3)/cvx_obj3
+if ratio3>1E-6
+    error('Unacceptable objective ratio');
+end
 
 % test with l1 sparsity and elastic net lambdas, and intercept
 opts.intercept = true;
-[ objs, ~, lambdas, ~, betas ] = eval_grplasso( Y, [X, ones(N,1)], ginds, opts);
+[ objs, ~, lambdas, ~, betas ] = eval_grplasso( Y, X, ginds, opts);
 
 rli = randi(length(lambdas));
 lambda = lambdas(rli);
@@ -99,14 +107,17 @@ lambdae = opts.lambdae;
 cvx_begin quiet
     variables betal(msize,p) b(1,1)
 
-    minimize( .5*sum(sum_square(Y-Xpad*betal(:)-b)) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) ...
+    minimize( -Y'*(Xpad*betal(:)+b) +sum(log_sum_exp([zeros(1,N); betal(:)'*Xpad'+b])) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) ...
                 + lambda1*sum(abs(betal(:))) + (lambdae/2)*sum_square(betal(:)) )
 
     subject to
         betal(npadind) == 0
 cvx_end
 
-cvx_obj4 = .5*sum(sum_square(Y-Xpad*betal(:)-b)) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) ...
+cvx_obj4 = -Y'*(Xpad*betal(:)+b) +sum(log_sum_exp([zeros(1,N); betal(:)'*Xpad'+b])) + lambda*dot(sqrt(gsizes),norms(betal,2,1)) ...
                 + lambda1*sum(abs(betal(:))) + (lambdae/2)*sum_square(betal(:));
 opt_obj4 = objs(rli);
-abs(opt_obj4-cvx_obj4)/cvx_obj4
+ratio4 = abs(opt_obj4-cvx_obj4)/cvx_obj4
+if ratio4>1E-6
+    error('Unacceptable objective ratio');
+end
